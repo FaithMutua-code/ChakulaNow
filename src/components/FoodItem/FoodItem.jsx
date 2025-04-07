f42163616d52be45cf147baedbab37e12a1608b5
FaithMutua-code <charlesfaith157@gmail>
2025-04-07 19:56:04 +0300

    Cart

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,37 +1,54 @@
-import React, { useState } from 'react';
+import { useContext } from 'react';
+import { StoreContext } from '../../context/StoreContext';
 import './FoodItem.css';
-import { assets } from '../../assets/frontend_assets/assets';
+import { assets } from '../../assets/frontend_assets/assets.js';
 
-const FoodItem = ({ name, price, description, image }) => {
-  const [itemCount, setItemCount] = useState(0);
+const FoodItem = ({ id, name, price, description, image }) => {
+  const [cartItem, addToCart, removeFromCart] = useContext(StoreContext);
+
+  const handleAdd = () => addToCart(id);
+  const handleRemove = () => removeFromCart(id);
+
+  const itemInCart = cartItem[id];
 
   return (
     <div className="food-item">
       <div className="food-item-img-container">
-        <img src={image} alt="" className="food-item-image" />
-        
-        { 
-          !itemCount 
-          ? <img 
+        <img src={image} alt={name} className="food-item-image" />
+        {
+          !itemInCart 
+          ? (
+            <img 
               className="add" 
-              onClick={() => setItemCount(prev => prev + 1)} 
+              onClick={handleAdd} 
               src={assets.add_icon_white} 
-              alt="Add to cart" 
+              alt="Add item to cart" 
             />
-          : <div className="food-item-counter">
-          <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
-            <p>{itemCount}</p>
-            <img  onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt="" />
+          ) 
+          : (
+            <div className="food-item-counter">
+              <img 
+                onClick={handleRemove} 
+                src={assets.remove_icon_red} 
+                alt="Remove item from cart" 
+              />
+              <p>{itemInCart}</p>
+              <img 
+                onClick={handleAdd} 
+                src={assets.add_icon_white} 
+                alt="Add item to cart" 
+              />
             </div>
+          )
         }
       </div>
-      
+
       <div className="food-item-info">
         <div className="food-item-name-rating">
           <p>{name}</p>
           <img src={assets.rating_starts} alt="Rating" />
         </div>
-        
+
         <p className="food-item-desc">{description}</p>
         <p className="food-item-price">Sh {price}</p>
       </div>
--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,5 +1,5 @@
 // src/context/StoreContext.js
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/frontend_assets/assets";  // Importing food_list
 
 // Create the context
@@ -7,9 +7,25 @@ const StoreContext = createContext(null);
 
 // Create the provider component
 export const StoreContextProvider = (props) => {
-  // Setting up the value for the context
+ const [cartItem,setCartItems]=useState({});
+ const addToCart=(itemId) =>{
+  if(!cartItem[itemId]){
+    setCartItems((prev)=>({...prev,[itemId]:1}))
+   
+  }
+  else{
+    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]}+1))/* if prod is already in the cart it will be increased by 1*/
+  }
+ }
+ const removeFromcart =(itemId) =>{
+  setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+ }
   const contextValue = {
     food_list,
+    cartItem,
+    setCartItems,
+    addToCart,
+    removeFromcart
   };
 
   return (
